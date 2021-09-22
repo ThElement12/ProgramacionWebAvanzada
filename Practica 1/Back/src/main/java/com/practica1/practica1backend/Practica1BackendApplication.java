b587bc1da9f690173e9feaff4412c0aa46f08946
ThElement12 <20170861@ce.pucmm.edu.do>
2021-09-22 09:20:00 -0400

    Se inicializan los proyectos

---
+++ Practica 1/Back/src/main/java/com/practica1/practica1backend/Practica1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.practica1.practica1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica1BackendApplication.class, args);
+    }
+
+}
---
+++ Practica 1/Back/src/test/java/com/practica1/practica1backend/Practica1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica1.practica1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
