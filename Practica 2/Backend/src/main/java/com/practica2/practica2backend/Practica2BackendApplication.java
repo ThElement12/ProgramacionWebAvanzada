4600c4edcb7e5de1b8494e22b327cbd2a199cc72
ThElement12 <20170861@ce.pucmm.edu.do>
2021-09-29 12:21:53 -0400

    Se inicializa el back y el front de la practica 2

---
+++ Practica 2/Backend/src/main/java/com/practica2/practica2backend/Practica2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.practica2.practica2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica2BackendApplication.class, args);
+    }
+
+}
---
+++ Practica 2/Backend/src/test/java/com/practica2/practica2backend/Practica2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica2.practica2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
