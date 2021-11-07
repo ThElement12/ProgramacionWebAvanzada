6c423ba721225db92a76e60334790de9a3d80929
ThElement12 <20170861@ce.pucmm.edu.do>
2021-11-07 16:59:28 -0400

    Se crea el dockerfile de los sensores

--- Practica 4/Cliente-1/index.js
+++ Practica 4/Cliente-1/index.js
@@ -1,79 +1,28 @@
-/*const express = require('express');
-const resultado = require('./request-api');
-const cors = require('cors');
-
-const app = express();
-const router = express.Router();
-require('dotenv').config();
-
-const TIEMPO = process.env.TIEMPO || 2;
-
-app.set('port', process.env.PORT || 5000)
-app.use(express.json());
-app.use(cors())
-
-
-app.use(
-  router.get('/', (req, res) => {
-    res.send("Hola desde el cliente 1");
-  })
-)
-
-app.listen(app.get('port'), () => {
-  setInterval(resultado, TIEMPO * 1000)
-  console.log("Cliente 1 en linea en el puerto:", app.get('port'));
-  console.log(`Enviando info cada ${TIEMPO} segundos`);
-
-})
-*/
-
-/*client.on("error", function (error) {
-  console.log("Can't connect" + error);
-  process.exit(1)
-});
-*/
-
 var mqtt = require('mqtt');
 options = {
   username: "admin",
   password: "admin"
 };
-var client = mqtt.connect("mqtt://localhost:1883")
+var client = mqtt.connect(process.env.MQT_ROUTE || 'mqtt://localhost:1883')
 client.on("connect", function () {
-
   console.log('connected!');
-
-  /*client.subscribe('sensor-sub', function() {
-    var msg = {
-      id:null,
-      idDevice:1,
-      generationDate:new Date().toLocaleString(),
-      temperature:Math.floor((Math.random() * 100)),
-      humidity:Math.floor((Math.random() * 100))
-    }
-    client.publish('sensor-sub', JSON.stringify(msg), {
-      retain: true,
-    });
-  });*/
-
 });
 
-var timer_id = setInterval(function () { publish( {
-  id:null,
-  idDevice:1,
-  generationDate:new Date().toLocaleString(),
-  temperature:Math.floor((Math.random() * 100)),
-  humidity:Math.floor((Math.random() * 100))
-}); }, 6000);
-
-
+var timer_id = setInterval(function () {
+  publish({
+    id: null,
+    idDevice: 1,
+    generationDate: new Date().toLocaleString(),
+    temperature: Math.floor((Math.random() * 100)),
+    humidity: Math.floor((Math.random() * 100))
+  });
+}, 6000);
 
 //publish function
 client.subscribe('sensor-sub')
 function publish(msg) {
   console.log("publishing", msg);
   if (client.connected == true) {
-    //client.publish("sensor-sub", JSON.stringify(msg));
     client.publish("sensor-sub", JSON.stringify(msg));
   }
 }
