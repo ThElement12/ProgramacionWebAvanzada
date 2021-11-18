bbe8ea57a00dee563b680c1dc16d05ad53718e6e
ThElement12 <20170861@ce.pucmm.edu.do>
2021-11-18 14:11:12 -0400

    Se importa bootstrap

--- Practica 5/frontend/src/Pages/Home/index.jsx
+++ Practica 5/frontend/src/Pages/Home/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import Reserv from '../../Components/Reserv';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default class Home extends Component {
   render() {
     return (
