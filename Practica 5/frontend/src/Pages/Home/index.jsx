d35ceb1d849db809d8fadc73d92844d66c528075
ThElement12 <20170861@ce.pucmm.edu.do>
2021-11-22 09:41:32 -0400

    Se le agrega el campo de hora

--- Practica 5/frontend/src/Pages/Home/index.jsx
+++ Practica 5/frontend/src/Pages/Home/index.jsx
@@ -21,16 +21,17 @@ export default function Home() {
   const [carrera, setCarrera] = useState("");
   const [laboratorio, setLaboratio] = useState("");
   const [fecha, setFecha] = useState("");
+  const [hora, setHora] = useState(8)
 
   useEffect(() => {
     setReserv([
-      new Reservation(1, "Joseph", "ISC", "ING", new Date(2021,7,12,12,30)),
-      new Reservation(2, "Robert", "ISC", "ING", new Date(2021,7,12,12,30)),
-      new Reservation(3, "Alicia", "ISC", "ING", new Date(2021,7,12,12,30)),
-      new Reservation(4, "Ruben", "ISC", "ING", new Date(2021,7,12,12,30)),
-      new Reservation(5, "Lendry", "ISC", "ING", new Date(2021,7,12,12,30)),
-      new Reservation(6, "Francisco", "ISC", "ING", new Date(2021,7,12,12,30)),
-      new Reservation(7, "Ariel", "ISC", "ING", new Date(2021,7,12,12,30))
+      new Reservation(1, "Joseph", "ISC", "ING", new Date(2021, 7, 12, 12, 30)),
+      new Reservation(2, "Robert", "ISC", "ING", new Date(2021, 7, 12, 12, 30)),
+      new Reservation(3, "Alicia", "ISC", "ING", new Date(2021, 7, 12, 12, 30)),
+      new Reservation(4, "Ruben", "ISC", "ING", new Date(2021, 7, 12, 12, 30)),
+      new Reservation(5, "Lendry", "ISC", "ING", new Date(2021, 7, 12, 12, 30)),
+      new Reservation(6, "Francisco", "ISC", "ING", new Date(2021, 7, 12, 12, 30)),
+      new Reservation(7, "Ariel", "ISC", "ING", new Date(2021, 7, 12, 12, 30))
     ])
   }, [])
 
@@ -41,7 +42,7 @@ export default function Home() {
   }
   const submitReserva = e => {
     e.preventDefault();
-    
+
     console.log(new Reservation(id, nombre, carrera, laboratorio, fecha));
     Swal.fire(
       'Registrado!',
@@ -52,13 +53,14 @@ export default function Home() {
     setShowRegister(false);
   }
   const handleClose = () => setShowRegister(false);
-  
+
   const clean = () => {
     setId("");
     setNombre("");
     setCarrera("");
     setLaboratio("");
     setFecha("");
+    setHora(8);
   }
 
   const modalRegister = () => {
@@ -69,22 +71,44 @@ export default function Home() {
         </Modal.Header>
         <Form onSubmit={submitReserva}>
           <Modal.Body>
-            <Form.Label>ID</Form.Label>
-            <Form.Control placerholder="ID" value={id} onChange={(e)=> {setId(e.target.value)}} required/>
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control placeholder="Nombre" value={nombre} onChange={(e)=> {setNombre(e.target.value)}} required/>
-            <Form.Label>Carrera</Form.Label>
-            <Form.Control placeholder="Carrera"  value={carrera} onChange={(e)=> {setCarrera(e.target.value)}} required/>
-            <Form.Label>Laboratorio</Form.Label>
-            <Form.Control placeholder="Laboratorio" value={laboratorio} onChange={(e)=> {setLaboratio(e.target.value)}} required/>
-            <Form.Label>Fecha de Reserva</Form.Label>
-            <Form.Control type="date" value={fecha} onChange={(e)=> {setFecha(e.target.value)}} required/>
+            <Form.Group>
+              <Form.Label>ID</Form.Label>
+              <Form.Control placerholder="ID" value={id} onChange={(e) => { setId(e.target.value) }} required />
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control placeholder="Nombre" value={nombre} onChange={(e) => { setNombre(e.target.value) }} required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Carrera</Form.Label>
+              <Form.Control placeholder="Carrera" value={carrera} onChange={(e) => { setCarrera(e.target.value) }} required />
+              <Form.Label>Laboratorio</Form.Label>
+              <Form.Control placeholder="Laboratorio" value={laboratorio} onChange={(e) => { setLaboratio(e.target.value) }} required />
+            </Form.Group>
+            {' '}
+            <Form.Group>
+              <Row>
+                <Col>
+                  <Form.Label>Fecha de Reserva</Form.Label>
+                </Col>
+                <Col>
+                  <Form.Control type="date" value={fecha} onChange={(e) => { setFecha(e.target.value) }} required />
+                </Col>
+              </Row>
+              <br></br>
+              <Row>
+                <Col>
+                  <Form.Label>Hora de Reserva: (24 Hrs) </Form.Label>
+                </Col>
+                <Col>
+                  <Form.Control type="number" value={hora} min={8} max={21} onChange={(e) => { setHora(e.target.value) }}/>
+                </Col>
+              </Row>
+            </Form.Group>
           </Modal.Body>
           <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button variant="primary" type="submit">Registrar</Button>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancelar
+            </Button>
+            <Button variant="primary" type="submit">Registrar</Button>
           </Modal.Footer>
         </Form>
       </Modal>
@@ -96,9 +120,9 @@ export default function Home() {
       <Container>
         <Form onSubmit={onSubmit}>
           <Row>
-            <Col><Form.Control type="date" size="sm" name="startDate" value={startDate} onChange={(e) => {setStartDate(e.target.value)}} required /></Col>
+            <Col><Form.Control type="date" size="sm" name="startDate" value={startDate} onChange={(e) => { setStartDate(e.target.value) }} required /></Col>
             a
-            <Col><Form.Control type="date" size="sm" name="finishDate" value={finishDate} onChange={(e) => {setfinishDate(e.target.value)}} required /></Col>
+            <Col><Form.Control type="date" size="sm" name="finishDate" value={finishDate} onChange={(e) => { setfinishDate(e.target.value) }} required /></Col>
             <Col><Button variant="outline-info" size="sm" type="submit">Buscar</Button></Col>
           </Row>
         </Form>
@@ -110,7 +134,7 @@ export default function Home() {
   return (
     <Container>
       <Container>
-        <Button variant="outline-primary" onClick={() => { setShowRegister(true)}}>Registrar</Button>{' '}
+        <Button variant="outline-primary" onClick={() => { setShowRegister(true) }}>Registrar</Button>{' '}
         <Button variant="outline-secondary" onClick={() => { sethideDates(false) }}>Historial</Button>
       </Container>
       <br></br>
@@ -118,7 +142,7 @@ export default function Home() {
         {!hideDates && datePickers()}
       </Container>
       <br></br>
-      <Reserv reservations={reserv}/>
+      <Reserv reservations={reserv} />
       {modalRegister()}
     </Container>
   )
