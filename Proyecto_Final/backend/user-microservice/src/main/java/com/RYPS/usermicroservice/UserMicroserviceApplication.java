929b1518d05623dbb8624da96fcd20652931c937
waterpolord <r_rokstar@hotmail.com>
2021-12-27 15:11:40 -0400

    se cambia el archivo de configuracion

--- Proyecto_Final/backend/user-microservice/src/main/java/com/RYPS/usermicroservice/UserMicroserviceApplication.java
+++ Proyecto_Final/backend/user-microservice/src/main/java/com/RYPS/usermicroservice/UserMicroserviceApplication.java
@@ -5,11 +5,13 @@ import com.RYPS.usermicroservice.models.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Collections;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserMicroserviceApplication {
 
