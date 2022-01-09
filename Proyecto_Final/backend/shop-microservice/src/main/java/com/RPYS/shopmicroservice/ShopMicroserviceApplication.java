c113eb010b970e243948180dbc18081ba4bcb528
waterpolord <robertgarcia341@gmail.com>
2022-01-09 14:53:15 -0400

    arreglando configuracion api-gateway

--- Proyecto_Final/backend/shop-microservice/src/main/java/com/RPYS/shopmicroservice/ShopMicroserviceApplication.java
+++ Proyecto_Final/backend/shop-microservice/src/main/java/com/RPYS/shopmicroservice/ShopMicroserviceApplication.java
@@ -7,6 +7,7 @@ import com.RPYS.shopmicroservice.repositories.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.*;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class ShopMicroserviceApplication {
