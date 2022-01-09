b8c4f5e068eabbaa9a4cc4925b5ff5e5a77c2024
waterpolord <robertgarcia341@gmail.com>
2022-01-09 15:38:51 -0400

    arreglando configuracion api-gateway

--- Proyecto_Final/backend/user-microservice/src/main/java/com/RYPS/usermicroservice/UserMicroserviceApplication.java
+++ Proyecto_Final/backend/user-microservice/src/main/java/com/RYPS/usermicroservice/UserMicroserviceApplication.java
@@ -5,6 +5,7 @@ import com.RYPS.usermicroservice.models.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 @EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserMicroserviceApplication {
 
