bfde7b9d207c233c9bf0d0c4c9945bd92e99c7fd
waterpolord <robertgarcia341@gmail.com>
2022-01-09 22:57:23 -0400

    cambiando cors en micro servicios

--- Proyecto_Final/backend/servidor-perimetral/final/src/main/java/edu/pucmm/servidorperimetralzuul/ServidorPerimetralZuulApplication.java
+++ Proyecto_Final/backend/servidor-perimetral/final/src/main/java/edu/pucmm/servidorperimetralzuul/ServidorPerimetralZuulApplication.java
@@ -5,6 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -39,4 +51,5 @@ public class ServidorPerimetralZuulApplication {
 
         return new CorsFilter(source);
     }
+
 }
